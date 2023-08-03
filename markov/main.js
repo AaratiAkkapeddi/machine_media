c7b750e98cef44592cc379f78d45feebbf9cc3cf
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-08-02 22:24:51 -0400

    adds markov

---
+++ markov/main.js
@@ -0,0 +1,34 @@
+// An array of lines from a text file
+let lines;
+// The Markov Generator object
+let markov;
+// An output element
+let output;
+
+// Preload some seed data
+
+function setup() {
+  // Join everything together in one long string
+  // Keep carriage returns so these will show up in the markov generator
+ 
+  //console.log(markov);
+  
+  // Make the button
+  let button = createButton('generate');
+  button.mousePressed(generate);
+
+  noCanvas();
+}
+
+function generate() {
+  // Generate some text
+  let text = document.querySelector("textarea").value;
+  text.replace(/[\n\r]/g, '');
+  // N-gram length and maximum length
+  markov = new MarkovGenerator(8, 1000);
+  markov.feed(text);
+  let result = markov.generate();
+  // Put in HTML line breaks wherever there was a carriage return
+  result = result.replace('\n','<br/><br/>');
+  document.querySelector("#result").innerHTML = "<strong>Result:</strong><br> <br>" + result;
+}
